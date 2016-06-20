6bae813423de6d28e30600e7a7c5acb130c6c2b8
swong92 <swong@copperleaf.com>
2016-06-20 09:35:25 -0700

    costest-203 wip

---
+++ utils/loopback-pre-boot-hal/index.js
@@ -0,0 +1,23 @@
+module.exports = ( server )=>{
+
+  return new Promise( ( resolve, reject )=>{
+
+    const Hypermedia = require( './utils/hypermedia' );
+    const boot       = require( 'loopback-boot' );
+
+    server.Hypermedia = Hypermedia;
+
+    boot( server, __dirname, ( err )=>{
+
+      if( err ) reject( err );
+
+      resolve();
+
+    } );
+
+  } );
+
+};
+
+
+
