c15199cb1c1929f63bede8799d5f01cb6463cfbb
Markus Westerholz <mwesterholz@gmail.com>
2016-10-07 15:09:14 -0700

    Add helper to simplify uit loading

--- run-tests.js
+++ run-tests.js
@@ -2,7 +2,7 @@ if ( require.main === module ){
 
   if( process.argv[ 2 ] === 'continuously' ){
 
-    const root = require( 'path').relative( process.cwd(), __dirname) || '.';
+    const root = require( 'path' ).relative( process.cwd(), __dirname ) || '.';
 
     require( 'child_process' ).fork( `${ root }/utils/lint-and-test.js` )
       .on( 'close', require( './utils/start-tdd-watch-loop' ) );
---
+++ spec/common-helpers/register-common-test-helpers.js
@@ -0,0 +1,20 @@
+//  To make these tools available in your project, add
+//  '../node_modules/cl-node-dev-tools/spec/common-helpers/**/*.js'
+//  to your helpers list in spec/support/jasmine.json
+
+const testUtils = {
+
+  // Usage: From your spec file, call
+  //
+  //   const uit
+  //     = process.cl_test_util.generateUITPath( __filename );
+  //
+  // See spec/test-util/generate-uit-path.js for more info
+  generateUITPath: require( '../test-util/generate-uit-path' )
+
+};
+
+process.cl_test_util
+  = process.cl_test_util
+  = Object.assign( ( process.cl_test_util || {} ), testUtils );
+
